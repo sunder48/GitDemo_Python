578f6aff62c1f8e06380cc4a890cfa150308c47d
sunder48 <singh.sunder147@gmail.com>
2025-10-07 15:48:51 +0530

    uploaded the task1 and Task 2 script for review

---
+++ Assignment1_Tasks.py
@@ -0,0 +1,14 @@
+
+a=input('Enter a first number = ')
+a=int(a)
+b=input('Enter a second number= ')
+b=int(b)
+Addition=a+b
+Subtraction=a-b
+Multiplication=a*b
+division=a/b
+print('Addition =',Addition)
+print('Substraction =',Subtraction)
+print('Multiplication =',Multiplication)
+print('division =',division)
+
